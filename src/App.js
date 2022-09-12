e294482024767c47c37e012a565eab2855b61ea8
focus110 <iamemmaochuko@gmail.com>
2022-09-12 11:22:01 +0100

    restructuring code to use tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/home';
-import Wallet from './pages/wallet';
-import Header from './components/header';
-import Dispose from './pages/dispose';
-import Dashboard from './pages/dashboard';
-import Collect from './pages/collect';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from './pages/home';
+// import Wallet from './pages/wallet';
+// import Header from './components/header';
+// import Dispose from './pages/dispose';
+import Dashboard from "./pages/dashboard";
+// import Collect from './pages/collect';
 function App() {
   return (
-    
     <BrowserRouter>
-      <Header/>
+      {/* <Header /> */}
       <Routes>
-        <Route path='/' element= {<Home/>} />
+        {/* <Route path='/' element= {<Home/>} />
         <Route path='wallet' element ={<Wallet/>} />
         <Route path='*' element = { <h2>oops nothing here</h2>} />
-        <Route path='dispose' element = {<Dispose/>}/>
-        <Route path='dashboard' element ={<Dashboard/>} />
-        <Route path='collect' element={<Collect />} />
+        <Route path='dispose' element = {<Dispose/>}/> */}
+        <Route path="dashboard" element={<Dashboard />} />
+        {/* <Route path='collect' element={<Collect />} /> */}
       </Routes>
     </BrowserRouter>
   );
