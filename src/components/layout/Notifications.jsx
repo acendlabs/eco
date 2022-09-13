0ce2250f3807b3cde15d75bc17f4a8f166ba3039
focus110 <iamemmaochuko@gmail.com>
2022-09-13 23:37:39 +0100

    notification comp updated

--- src/components/layout/Notifications.jsx
+++ src/components/layout/Notifications.jsx
@@ -7,7 +7,7 @@ const Notifications = () => {
       <h2 className="text-2xl font-bold mt-8 mb-8 md:mb-16 space-x-8">
         Notifications
       </h2>
-      <div className="w-full h-auto p-2 md:p-16 rounded-xl bg-[#EFF6F2] space-y-4 py-8">
+      <div className="w-full h-auto p-2 md:px-8 pt-8 pb-20 rounded-xl bg-[#EFF6F2] space-y-4">
         {notifications.map((item, i) => {
           return (
             <div
