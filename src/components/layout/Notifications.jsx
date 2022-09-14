35a21948a3461457863b2b3a298a84994a85f4cc
focus110 <iamemmaochuko@gmail.com>
2022-09-14 16:46:24 +0100

    hide scroll bar tailwind plugin

--- src/components/layout/Notifications.jsx
+++ src/components/layout/Notifications.jsx
@@ -7,7 +7,7 @@ const Notifications = () => {
       <h2 className="text-2xl font-bold mt-8 mb-8 md:mb-16 space-x-8">
         Notifications
       </h2>
-      <div className="w-full h-auto p-2 md:px-8 pt-8 pb-16 rounded-xl bg-[#EFF6F2] space-y-4">
+      <div className="w-full h-auto py-2 px-2.5 md:px-8 pt-8 pb-16 rounded-xl bg-[#EFF6F2] space-y-4">
         {notifications.map((item, i) => {
           return (
             <div
@@ -25,7 +25,7 @@ const Notifications = () => {
               </p>
               <div>
                 <img
-                  className="bg-acend-theme-dark w-8 h-8 sm:w-11 sm:h-11 rounded-full"
+                  className="cursor-pointer bg-acend-theme-dark w-8 h-8 sm:w-11 sm:h-11 rounded-full"
                   src="https://images.unsplash.com/photo-1662848586769-314289d4c8b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60"
                   alt="avatar"
                 />
